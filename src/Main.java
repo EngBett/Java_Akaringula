2e20f8f1d596e271897587003248c1e6e0e1a0eb
EngBett <bettthetimetraveller@gmail.com>
2020-07-08 16:15:10 +0300

    Optimized code

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
 
     public static void main(String[] args) throws InterruptedException {
-        Akaringula akaringula = new Akaringula(10,3);
+        Akaringula akaringula = new Akaringula(9,3);
         akaringula.Play();
     }
 
